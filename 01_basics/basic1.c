393773cfdae37cde4914f1c974a6dc74f3518b67
Parth Gupta <06082004parthgupta@gmail.com>
2024-08-11 01:59:59 +0530

    created basics

---
+++ 01_basics/basic1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    // ctrl +  1 + `
+    // terminal clear -> terminal band bhi ho jati hai
+    // terminal toggle ( khulega or band hojayega )
+    // ctrl + ` + ctrl + `
+    // `
+    // code run 
+    // keyboard shortcuts
+    // ctrl + k + s
+
+    // alt + q + enter
+    // code format
+
+
+    printf("Hi Anisha");
+    // ctrl + d -> copy line down
+    printf("Hi Anisha");
+    return 0;
+}
